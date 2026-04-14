43a163386f219d3c9265ee3f8b733b41b6d6834e
nilig12 <144149690+nilig12@users.noreply.github.com>
2026-04-14 18:43:50 +0530

    Update authService.js

--- src/features/auth/authService.js
+++ src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = process.env.API_URL 
+const API_URL = import.meta.env.API_URL
 
 const api = axios.create({
     baseURL: API_URL,
@@ -11,12 +11,12 @@ const api = axios.create({
 
 
 export const registerUser = async (userData) => {
-    const response = await api.post(`${import.meta.env.API_URL}/api/auth/register`, userData)
+    const response = await api.post(`/api/auth/register`, userData)
     return response.data;
 }
 
 export const loginUser = async (userData) => {
-    const response = await api.post(`${import.meta.env.API_URL}/api/auth/login`, userData)
+    const response = await api.post(`/api/auth/login`, userData)
     return response.data;
 }
 
